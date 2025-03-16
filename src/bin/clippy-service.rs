b4882b9e37403a6eaa1fdf6f4cc73b845c7e2eaf
dhanushl0l <dhanushdjtherock@gmail.com>
2025-03-17 00:08:15 +0530

    added to specific fan for compatibility

--- src/bin/clippy-service.rs
+++ src/bin/clippy-service.rs
@@ -1,31 +1,36 @@
 use clipboard_rs::{ClipboardWatcher, ClipboardWatcherContext};
 use clippy::read_clipboard::{self, read_wayland_clipboard};
+use std::error::Error;
 use std::{env, process};
+
+#[cfg(target_os = "linux")]
 use wayland_clipboard_listener::{WlClipboardPasteStream, WlListenType};
 
 fn main() {
-    match env::consts::OS {
-        "linux" => {
-            if env::var("WAYLAND_DISPLAY").is_ok() {
-                read_wayland()
-            } else if env::var("DISPLAY").is_ok() {
-                read();
-            } else {
-                eprint!("No display server detected");
+    run()
+}
+
+#[cfg(target_os = "linux")]
+fn run() {
+    if env::var("WAYLAND_DISPLAY").is_ok() {
+        read_clipboard_wayland()
+    } else if env::var("DISPLAY").is_ok() {
+        match read_clipboard() {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Failed to initialize clipboard listener\n{}", err);
                 process::exit(1);
             }
-        }
-        "windows" => read(),
-        "macos" => read(),
-        _ => {
-            eprintln!("unsuported hardware");
-            process::exit(1);
-        }
+        };
+    } else {
+        eprint!("No display server detected");
+        process::exit(1);
     }
 }
 
 // need to find a way to monitor clipboard changes in wayland the current way is not optimized
-fn read_wayland() {
+#[cfg(target_os = "linux")]
+fn read_clipboard_wayland() {
     let mut stream = WlClipboardPasteStream::init(WlListenType::ListenOnCopy).unwrap();
 
     for _ in stream.paste_stream().flatten().flatten() {
@@ -36,10 +41,27 @@ fn read_wayland() {
     }
 }
 
-fn read() {
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+fn run() {
+    match read_clipboard() {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("Failed to initialize clipboard listener\n{}", err);
+            process::exit(1);
+        }
+    };
+}
+
+#[cfg(not(any(target_os = "windows", target_os = "linux", target_os = "macos")))]
+fn run() {
+    eprintln!("Unsupported OS");
+    process::exit(1);
+}
+
+fn read_clipboard() -> Result<(), Box<dyn Error + Send + Sync>> {
     let manager = read_clipboard::Manager::new();
 
-    let mut watcher = ClipboardWatcherContext::new().unwrap();
+    let mut watcher = ClipboardWatcherContext::new()?;
 
     let _watcher_shutdown = watcher.add_handler(manager).get_shutdown_channel();
 
@@ -47,4 +69,5 @@ fn read() {
         println!("start watch!");
     }
     watcher.start_watch();
+    Ok(())
 }
